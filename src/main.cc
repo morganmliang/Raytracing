93f58dc0bec132d427b350f5ed35ddcc97c23e53
Your Name <your@example.com>
2022-03-27 14:02:10 +1100

    Revert "add ray color and aspect ratio to main"
    This reverts commit 4b74b3915b6594532c431c9be5098e53fda655df.

--- src/main.cc
+++ src/main.cc
@@ -1,38 +1,17 @@
 #include "color.h"
-#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
 
-color ray_color(const ray& r) {
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
-}
-
-
 int main() {
-    const auto aspect_ratio = 16.0 / 9.0;
-    const auto image_width = 400;
-    const auto image_height = static_cast<int>(image_width / aspect_ratio);
-
-    //Camera
-
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0,0,0);
-    auto horizontal = vec3(viewport_width,0,0);
-    auto vertical = vec3(0,viewport_height, 0);
-    auto lower_left_corner = origin-horizontal/2 - vertical/2 - vec3(0,0, focal_length);
+    const auto image_width = 256;
+    const auto image_height = 256;
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 
     for (auto j = image_height - 1; j >= 0;  --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (auto i = 0; i < image_width; ++i){
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
             color pixel_color(double(i)/(image_width - 1), double(j) /(image_height - 1), 0.25);
             write_color(std::cout, pixel_color);
         }

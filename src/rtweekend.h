449232932ecee7810621428569cc908c0b819e1b
Your Name <your@example.com>
2022-03-31 17:18:58 +1100

    Add antialiasing and diffuse materials

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -4,13 +4,29 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
+
 
 
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1516926535897932385;
 
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
 inline double degrees_to_radians(double degrees) {
     return degrees * pi /180.0;
+}
+
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
 }
 
 #include "ray.h"

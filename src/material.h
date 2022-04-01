4e7f9ddb6544a0c400037546de70355632381fb4
Your Name <your@example.com>
2022-04-01 15:23:35 +1100

    add metal class

--- src/material.h
+++ src/material.h
@@ -31,6 +31,20 @@ class lambertian : public material {
         color albedo;
 };
 
+class metal : public material {
+    public:
+        metal(const color& a): albedo(a) {}
+
+        virtual bool scatter (const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected);
+            attenuation = albedo;
+            return (dor(scattered.direction(), rec.normal) > 0);
+        }
+    public:
+        color albedo;
+};
+
 
 #endif
 
